8bf852204c52d84737deb0459cd001d90772f81e
Home <githubmails@gmx.net>
2021-06-11 15:52:19 +0200

    Split method argument: methods for sieve, divisors

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 #
-# Eratosthenes v0.7 2021-05-06
+# Eratosthenes v0.8 2021-06-11
 
 # Version
 
-version_num = '0.7'
-version_dat = '2021-05-06'
+version_num = '0.8'
+version_dat = '2021-06-11'
 version_str = '{} ({})'.format(version_num, version_dat)
 
 # Import modules
@@ -13,6 +13,7 @@ version_str = '{} ({})'.format(version_num, version_dat)
 import argparse
 import time
 import numpy as np
+import sieves
 
 # Define argument parsers and subparsers
 
@@ -24,14 +25,15 @@ parser.add_argument('-v', '--verbose', action='count', default=0,
                     'default = single-line output, v = multi-line, vv = detailed, vvv = array output')
 parser.add_argument('-q', '--quiet', action='store_true',
                     help=('disable terminal output (terminates all verbosity)'))
-parser.add_argument('-m', '--method', dest='method', choices=('all', 'sqrt', 'odd-all', 'odd-sqrt', '6k-all', '6k-sqrt', '4k-all', '4k-sqrt', '3k-all', '3k-sqrt', 'divisors'), default='sqrt', help='sieve method')
+parser.add_argument('-d', '--divisormethod', dest='divisormethod', choices=('all', 'sqrt'), default='sqrt', help='divisor method')
+parser.add_argument('-s', '--sievemethod', dest='sievemethod', choices=('all', 'odd', '3k', '4k', '6k'), default='6k', help='sieve method')
 parser.add_argument('limit', type=int, default=100, help='upper limit of test range')
 parser.add_argument('outfile', nargs='?', help='write to file')
 
 args = parser.parse_args()
 
-# Check verbosity level
 
+# Check verbosity level
 verbosity = args.verbose
 if args.quiet is True:
     verbosity = -1
@@ -39,225 +41,30 @@ if verbosity >= 1:
     print(args)
 
 
-# Determine divisors of a number
-
-def divisorsfast(number):
-    """
-    Determine all divisors of number up to square root of number
-    """
-    divs = []
-    divs.append(1)
-    for i in range(2, int(np.sqrt(number))+1):
-        if number % i == 0:
-            divs.append(i)
-    divs.append(number)
-    return divs
-
-
-def divisors(number):
-    """
-    Determine all divisors of number up to number
-    """
-    divs = []
-    divs.append(1)
-    for i in range(2, number):
-        if number % i == 0:
-            divs.append(i)
-    divs.append(number)
-    return divs
-
-
-# Sieve algorithms
-
-def alg1(end):
-    """
-    Check all numbers up to upper endpoint
-    """
-    prime = []
-    for i in range(2, end+1):
-        if len(divisors(i)) == 2:
-            prime.append(i)
-    return prime
-
-
-def alg2(end):
-    """
-    Check all numbers up to square root of upper endpoint
-    """
-    prime = []
-    for i in range(2, end+1):
-        if len(divisorsfast(i)) == 2:
-            prime.append(i)
-    return prime
-
-
-def alg3(end):
-    """
-    Check only odd numbers up to upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    for i in range(3, end+1, 2):
-        if len(divisors(i)) == 2:
-            prime.append(i)
-    return prime
-
-
-def alg4(end):
-    """
-    Check only odd numbers up to square root of upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    for i in range(3, end+1, 2):
-        if len(divisorsfast(i)) == 2:
-            prime.append(i)
-    return prime
-
-def alg5(end):
-    """
-    Check all numbers of form 6k-1 and 6k+1 up to upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-1)//6+1):
-        class1 = 6*i-1
-        class2 = 6*i+1
-        if len(divisors(class1)) == 2:
-            prime.append(class1)
-        if len(divisors(class2)) == 2:
-            prime.append(class2)
-    return prime
-
-
-def alg6(end):
-    """
-    Check all numbers of form 6k-1 and 6k+1 up to square root of upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-1)//6+1):
-        class1 = 6*i-1
-        class2 = 6*i+1
-        if len(divisorsfast(class1)) == 2:
-            prime.append(class1)
-        if len(divisorsfast(class2)) == 2:
-            prime.append(class2)
-        if i % 1000 == 0:
-            print('.', end='', flush=True)
-    print('')
-    return prime
-
-
-def alg7(end):
-    """
-    Check all numbers of form 4k+1 and 4k+3 up to upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-3)//4+1):
-        class1 = 4*i+1
-        class2 = 4*i+3
-        if len(divisors(class1)) == 2:
-            prime.append(class1)
-        if len(divisors(class2)) == 2:
-            prime.append(class2)
-    return prime
-
-
-def alg8(end):
-    """
-    Check all numbers of form 4k+1 and 4k+3 up to square root of upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-3)//4+1):
-        class1 = 4*i+1
-        class2 = 4*i+3
-        if len(divisorsfast(class1)) == 2:
-            prime.append(class1)
-        if len(divisorsfast(class2)) == 2:
-            prime.append(class2)
-    return prime
-
-
-def alg9(end):
-    """
-    Check all numbers of form 3k+1 and 3k+2 up to upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-2)//3+1):
-        class1 = 3*i+1
-        class2 = 3*i+2
-        if len(divisors(class1)) == 2:
-            prime.append(class1)
-        if len(divisors(class2)) == 2:
-            prime.append(class2)
-    return prime
-
-
-def alg10(end):
-    """
-    Check all numbers of form 3k+1 and 3k+2 up to square root of upper endpoint
-    """
-    prime = []
-    prime.append(2)
-    prime.append(3)
-    for i in range(1, (end-2)//3+1):
-        class1 = 3*i+1
-        class2 = 3*i+2
-        if len(divisorsfast(class1)) == 2:
-            prime.append(class1)
-        if len(divisorsfast(class2)) == 2:
-            prime.append(class2)
-    return prime
-
-
-def numdivisors(end):
-    dividends = np.arange(start=1, stop=end+1, dtype=int)
-    divisors = np.arange(start=1, stop=end+1, dtype=int)
-    for i in range(1, len(dividends)+1):
-        ndivisors = 0
-        for j in range(1, i//2+1):  # only check up to half
-            if i % j ==0:
-                ndivisors += 1
-        divisors[i-1] = ndivisors+1 # add 1 for dividend itself
-    new = np.column_stack([dividends, divisors])
-    return new
-
-
-
-# Main part
-
 end = args.limit
-method = args.method
+algorithm = sieves.Algorithm(args.divisormethod, args.sievemethod)
 outfile = args.outfile
 start = time.process_time()
-if method == 'all':
+
+if algorithm.sievemethod == 'all' and algorithm.divisormethod == 'all':
     primes = alg1(int(end))
-elif method == 'sqrt':
+elif algorithm.sievemethod == 'all' and algorithm.divisormethod == 'sqrt':
     primes = alg2(int(end))
-elif method == 'odd-all':
+elif algorithm.sievemethod == 'odd' and algorithm.divisormethod == 'all':
     primes = alg3(int(end))
-elif method == 'odd-sqrt':
+elif algorithm.sievemethod == 'odd' and algorithm.divisormethod == 'sqrt':
     primes = alg4(int(end))
-elif method == '6k-all':
+elif algorithm.sievemethod == '6k' and algorithm.divisormethod == 'all':
     primes = alg5(int(end))
-elif method == '6k-sqrt':
+elif algorithm.sievemethod == '6k' and algorithm.divisormethod == 'sqrt':
     primes = alg6(int(end))
-elif method == '4k-all':
+elif algorithm.sievemethod == '4k' and algorithm.divisormethod == 'all':
     primes = alg7(int(end))
-elif method == '4k-sqrt':
+elif algorithm.sievemethod == '4k' and algorithm.divisormethod == 'sqrt':
     primes = alg8(int(end))
-elif method == '3k-all':
+elif algorithm.sievemethod == '3k' and algorithm.divisormethod == 'all':
     primes = alg9(int(end))
-elif method == '3k-sqrt':
+elif algorithm.sievemethod == '3k' and algorithm.divisormethod == 'sqrt':
     primes = alg10(int(end))
 elif method == 'divisors':
     primes = numdivisors(int(end))
@@ -273,10 +80,11 @@ if method != 'divisors':
     if outfile is not None:
         with open(outfile, 'w', encoding='UTF-8') as f:
             f.write('# ********** Eratosthenes v{} **********\n'.format(version_str))
-            f.write('# Tested integer range:   [2, {}]\n'.format(end))
-            f.write('# Detected prime numbers: {}\n'.format(len(primes)))
-            f.write('# Applied method:         {}\n'.format(method))
-            f.write('# Sifting time:           {} seconds\n'.format(elapsed))
+            f.write('# Tested integer range:    [2, {}]\n'.format(end))
+            f.write('# Detected prime numbers:  {}\n'.format(len(primes)))
+            f.write('# Applied divisors method: {}\n'.format(divisorsmethod))
+            f.write('# Applied sieve method:    {}\n'.format(sievemethod))
+            f.write('# Sifting time:            {} seconds\n'.format(elapsed))
             f.write('# **************************************************\n')
             for i in range(len(primes)):
                 f.write('{}\n'.format(primes[i]))
@@ -286,9 +94,9 @@ else:
     if outfile is not None:
         with open(outfile, 'w', encoding='UTF-8') as f:
             f.write('# ********** Eratosthenes v{} **********\n'.format(version_str))
-            f.write('# Integer range:   [2, {}]\n'.format(end))
-            f.write('# Applied method:  {}\n'.format(method))
-            f.write('# Time:            {} seconds\n'.format(elapsed))
+            f.write('# Integer range:           [2, {}]\n'.format(end))
+            f.write('# Applied divisors method: {}\n'.format(divisorsmethod))
+            f.write('# Time:                    {} seconds\n'.format(elapsed))
             f.write('# **************************************************\n')
             f.write('# Number\tDivisors\n')
             for i in range(len(primes)):
